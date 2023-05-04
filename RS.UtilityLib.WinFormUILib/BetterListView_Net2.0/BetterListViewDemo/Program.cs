297db17777df0962e8800241635e7b641c154f76
andyhebear <andyhebear@163.com>
2023-05-04 11:41:21 +0800

    更新demo(远程桌面)

--- RS.UtilityLib.WinFormUILib/BetterListView_Net2.0/BetterListViewDemo/Program.cs
+++ RS.UtilityLib.WinFormUILib/BetterListView_Net2.0/BetterListViewDemo/Program.cs
@@ -1,20 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using WinRemoteDesktop;
 
 namespace BetterListViewDemo
 {
-  static class Program
-  {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    static class Program
     {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new Form1());
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            Application.Run(new FormMain());
+        }
     }
-  }
 }
