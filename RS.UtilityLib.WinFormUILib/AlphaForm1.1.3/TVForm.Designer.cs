2ff29ce4ef3b937ce2ad9340e153c247d52ec5d8
RainsSoft <andyhebear@163.com>
2025-12-17 18:04:17 +0800

    增加透明镂空窗口AlphaForm
    增加透明镂空窗口AlphaForm

---
+++ RS.UtilityLib.WinFormUILib/AlphaForm1.1.3/TVForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace AlphaForm
+{
+    partial class TVForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.alphaFormTransformer1 = new AlphaForm.AlphaFormTransformer();
+            this.SuspendLayout();
+            // 
+            // alphaFormTransformer1
+            // 
+            this.alphaFormTransformer1.BackgroundImage = global::AlphaForm.Properties.Resources.TVBgImage;
+            this.alphaFormTransformer1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.alphaFormTransformer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.alphaFormTransformer1.DragSleep = ((uint)(30u));
+            this.alphaFormTransformer1.Location = new System.Drawing.Point(0, 0);
+            this.alphaFormTransformer1.Name = "alphaFormTransformer1";
+            this.alphaFormTransformer1.Size = new System.Drawing.Size(520, 450);
+            this.alphaFormTransformer1.TabIndex = 0;
+            // 
+            // TVForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 450);
+            this.Controls.Add(this.alphaFormTransformer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TVForm";
+            this.Text = "TVForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AlphaFormTransformer alphaFormTransformer1;
+    }
+}
---
+++ RS.UtilityLib.WinFormUILib/AlphaForm1.1.3/TVForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AlphaForm
+{
+    public partial class TVForm : Form
+    {
+        public TVForm() {
+            InitializeComponent();
+        }
+        protected override void OnClosing(CancelEventArgs e) {
+            // On XP, the speed at which the main form fades is 
+            // distractingly slow because of its size (see my note in OnShown below),
+            // so we just fade out the layered window frame. 
+            // NOTE: If you have the main form with a background image,
+            // that matches for AFT's background image (i.e., controls on top),
+            // you'll always want to fade in/out both windows.
+
+            alphaFormTransformer1.Fade(FadeType.FadeOut, true,
+              System.Environment.OSVersion.Version.Major < 6, 500);
+
+            base.OnClosing(e);
+        }
+
+        protected override void OnShown(EventArgs e) {
+            // I'm not real pleased with the speed that XP fades in
+            // the main form when the form itself is somewhat large like
+            // this one. Apparently when you have a Region, changing
+            // the layered window opacity attribute doesn't draw very fast
+            // for large regions. So here we only fade in the layered window.
+            // NOTE: If you have the main form with a background image,
+            // that matches for AFT's background image (sans alpha channel),
+            // you'll always want to fade in/out both, windows otherwise 
+            // it will look ugly. Look at the ControlsOnTopOfSkin project
+            // where we've added calls to Fade().
+
+            alphaFormTransformer1.Fade(FadeType.FadeIn, false,
+             System.Environment.OSVersion.Version.Major < 6, 400);
+            base.OnShown(e);
+        }
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e); 
+            alphaFormTransformer1.TransformForm(0); // must pass 0 if we're fading in (set's layered window's opacity)
+        }
+       
+        private void skinButton2_Click(object sender, EventArgs e) {
+            //Bitmap bmap = TestFormSkinChange.Properties.Resources.skin2;
+            //alphaFormTransformer1.UpdateSkin(bmap, null, 255);
+        }
+
+        private void skinButton1_Click(object sender, EventArgs e) {
+            //Bitmap bmap = TestFormSkinChange.Properties.Resources.skin1;
+            //alphaFormTransformer1.UpdateSkin(bmap, null, 255);
+        }
+    }
+}
