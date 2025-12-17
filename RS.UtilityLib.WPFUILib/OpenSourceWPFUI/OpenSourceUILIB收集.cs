2b3ac5d118b1311d4f261542d3f51bfa81c42174
RainsSoft <andyhebear@163.com>
2025-12-17 18:11:53 +0800

    Create OpenSourceUILIB收集.cs

---
+++ RS.UtilityLib.WPFUILib/OpenSourceWPFUI/OpenSourceUILIB收集.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenSourceWPFUI
+{
+    public class OpenSourceUILIB收集
+    {
+        //WPF UI开源控件
+        //https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit
+        //https://github.com/Mochengvia/PanuonUI.Silver 支持.net4.0 不再维护
+        //https://github.com/Panuon/Panuon.WPF.UI
+        //https://github.com/aduskin/AduSkin
+        //https://github.com/HandyOrg/HandyControl
+
+        //WinForm UI开源控件
+        //https://gitee.com/yhuse/SunnyUI
+        //https://github.com/IgnaceMaes/MaterialSkin
+        //https://github.com/kwwwvagaa/NetWinformControl
+    }
+}
