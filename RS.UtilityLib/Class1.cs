df555f8a7f4ebb421bc4f0972abb966fbf309a9f
RainsSoft <andyhebear@163.com>
2025-12-17 17:58:45 +0800

    一些DLL
    一些DLL

--- RS.UtilityLib/Class1.cs
+++ RS.UtilityLib/Class1.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using RS.UtilityLib.SizeOfGenericT;
 namespace RS.UtilityLib.SizeOfGenericT
 {
     class Class1
     {
+        void test() {
+            MemoryBlock mb = new MemoryBlock(1024);
+            Axiom.Core.MemoryBuffer<int> a = Axiom.Core.MemoryManager.Instance.Allocate<int>(100);
+            Axiom.Core.MemoryManager.Instance.Initialize(a);
+        }
+
     }
 }
